50587098cc4da5965ea05661590c4a80d43df275
AnmolK0614 <anmol_k@cy.iitr.ac.in>
2022-01-09 01:22:14 +0530

    JAN

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,17 +4,19 @@ import './card.css';
 import workCardOne from '../Assets/work_card_one.png';
 import workCardTwo from '../Assets/work_card_two.png';
 import workCardThree from '../Assets/work_card_three.png';
-import workCardFour from '../Assets/work_card_four.png';
+import workCardLast from '../Assets/work_card_four.png';
+import workCardFive from '../Assets/work_card_five.png';
+import workCardSix from '../Assets/work_card_six.png';
 
 function Cards() {
     const projects = [
         {
-            imgUrl: workCardOne,
-            subtitle: 'Informative Blog',
-            name: 'AE Shader - Reflection on objects',
-            description: 'A simpler method to give shadows/reflections on different surfaces without keyframing and masking.',
-            button_name: 'Open medium',
-            url: 'https://medium.com/@anmol_k/shader-in-after-effects-143ce9161cb3'
+            imgUrl: workCardThree,
+            subtitle: 'Showreel',
+            name: 'Info graphic animations',
+            description: 'Creative storytelling through motion design.',
+            button_name: 'Play video',
+            url: 'https://drive.google.com/file/d/1nTqrpS_4tN6Kwvgr-9PXkTnifBtvne_V/view?usp=sharing'
         },
         { 
             imgUrl: workCardTwo,
@@ -25,21 +27,38 @@ function Cards() {
             url: 'https://www.notion.so/Eka-Care-Assests-266be9e50fe2458fae575141212fff2b'
         },
         {
-            imgUrl: workCardThree,
-            subtitle: 'Showreel',
-            name: 'Info graphic animations',
-            description: 'Creative storytelling through motion design.',
-            button_name: 'Play video',
-            url: 'https://drive.google.com/file/d/1nTqrpS_4tN6Kwvgr-9PXkTnifBtvne_V/view?usp=sharing'
+            imgUrl: workCardFive,
+            subtitle: 'Branding',
+            name: 'eSports IITR Branding',
+            description: 'Branding of the offical iit Roorkee group ESPORTS ITTR',
+            button_name: 'Open',
+            url: ''
         },
         {
-            imgUrl: workCardFour,
+            imgUrl: workCardSix,
+            subtitle: 'Motion design intern | Curelink',
+            name: '',
+            description: 'My exprience with curelink',
+            button_name: 'Read documentation',
+            url: 'https://www.behance.net/gallery/125324027/2021-Mid-year-work-showcase'
+        },
+        {
+            imgUrl: workCardOne,
+            subtitle: 'Informative Blog',
+            name: 'AE Shader - Reflection on objects',
+            description: 'A simpler method to give shadows/reflections on different surfaces without keyframing and masking.',
+            button_name: 'Open medium',
+            url: 'https://medium.com/@anmol_k/shader-in-after-effects-143ce9161cb3'
+        },
+        {
+            imgUrl: workCardLast,
             subtitle: 'Behance',
             name: 'More work?',
             description: 'Check out my more work on behance!',
             button_name: 'Open Behance',
             url: 'https://www.behance.net/gallery/125324027/2021-Mid-year-work-showcase'
         },
+        
 ]
     return (
         <div className='cards' id='works-container'>
